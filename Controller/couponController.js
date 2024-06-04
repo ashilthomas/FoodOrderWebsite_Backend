6e10917ed5d28a7ef65029eabb81a938f6e41ced
Ashil Thomas <ashilthomas31@gmail.com>
2024-06-04 12:46:54 +0530

    route update

--- Controller/couponController.js
+++ Controller/couponController.js
@@ -1,6 +1,30 @@
 import CouponModel from "../models/couponModel.js";
 
 
+const coupon = async(req,res)=>{
+    try {
+        
+        const coupon = await CouponModel.find({})
+
+        if(!coupon){
+            return res.json({
+                success:false,
+                message:"no coupon available"
+            })
+        }
+
+        res.status(200).json({
+            success:true,
+            coupon
+        })
+    } catch (error) {
+        res.status(404).json({
+            success:false,
+            message:"internal server error"
+        })
+    }
+}
+
 const coupanValidate = async(req,res)=>{
     const { code } = req.body;
     try {
@@ -14,9 +38,14 @@ const coupanValidate = async(req,res)=>{
         res.status(200).json(coupon)
     } catch (error) {
         console.log(error);
+        res.status(404).json({
+            success:false,
+            message:"internal server error"
+        })
     }
 }
 
 export {
-    coupanValidate 
+    coupanValidate,
+    coupon
 }
--- Routes/orderRoute.js
+++ Routes/orderRoute.js
@@ -1,8 +1,9 @@
 import express from "express"
 import { placeOrder } from "../Controller/orderController.js"
-import { coupanValidate } from "../Controller/couponController.js"
+import { coupanValidate, coupon } from "../Controller/couponController.js"
 const orderRoute = express.Router()
 
 orderRoute.post("/placeorder",placeOrder)
 orderRoute.post("/validatecoupon",coupanValidate)
+orderRoute.post("/coupon",coupon)
 export default orderRoute
