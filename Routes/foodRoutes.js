e3e2674b71e7290e47d6d40370d27f5238826c8d
Ashil Thomas <142432081+ashilthomas@users.noreply.github.com>
2025-10-31 23:45:56 +0530

    update code api

--- Routes/foodRoutes.js
+++ Routes/foodRoutes.js
@@ -1,7 +1,7 @@
 import express from "express"
 const foodRoute = express.Router()
 import upload from "../middleware/fileUploader.js"
-import { addFoodMenuItems, deletMenuItems, getCategoryItems, getallFoodMenuItems, itemFilter, pagination, restaurantItems, searchMenuItems, singleMenuItems } from "../Controller/foodController.js";
+import { addFoodMenuItems, deletMenuItems, getCategoryItems, getallFoodMenuItems, itemFilter, pagination, restaurantItems, searchMenuItems, singleMenuItems, menuItemCategory } from "../Controller/foodController.js";
 
 
 foodRoute.post("/addfood",upload.single("image"),addFoodMenuItems)
@@ -12,6 +12,7 @@ foodRoute.get("/itemsfilter",itemFilter)
 foodRoute.get("/categoryitems",getCategoryItems)
 foodRoute.get("/restaurantitems",restaurantItems)
 foodRoute.get("/singlemenuitems",singleMenuItems)
+foodRoute.get("/menuItemCategory",menuItemCategory)
 
 foodRoute.get("/pagination",pagination)
 export default foodRoute;
